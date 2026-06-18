95a9f1d956e5f08fe8ac5d5d9325a387b4b740fe
Sansskay <temirgalisanzar9@gmail.com>
2026-06-18 14:37:03 +0500

    fix next config syntax

--- next.config.ts
+++ next.config.ts
@@ -2,11 +2,9 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   eslint: {
-    // Это заставит Vercel пропустить проверку линтера при деплое
     ignoreDuringBuilds: true,
   },
   typescript: {
-    // Это пропустит ошибки типов TypeScript при сборке
     ignoreBuildErrors: true,
   },
 };
